49b8b18e2c13783cd71e8c46f00dc06273f02de3
Marcin Siwonia <mahinek12@gmail.com>
2023-07-23 15:31:18 +0200

    Add prisma endpoints for orders and products

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -22,18 +22,18 @@ export class OrdersController {
   }
 
   @Get('/:id')
-  public getById(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.ordersService.getById(id))
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
     return this.ordersService.getById(id);
   }
 
   @Delete('/:id')
-  public delete(@Param('id', new ParseUUIDPipe()) id: string) {
-    if (!this.ordersService.getById(id))
+  async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
 
-    this.ordersService.delete(id);
+    await this.ordersService.deleteById(id);
     return { success: true };
   }
 
@@ -43,14 +43,14 @@ export class OrdersController {
   }
 
   @Put('/:id')
-  update(
+  async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() orderData: UpdateOrderDTO,
   ) {
-    if (!this.ordersService.getById(id))
+    if (!(await this.ordersService.getById(id)))
       throw new NotFoundException('Order not found');
 
-    this.ordersService.updateById(id, orderData);
+    await this.ordersService.updateById(id, orderData);
     return { success: true };
   }
 }
