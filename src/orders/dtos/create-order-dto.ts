70b50f7f954d5e3d7dd293958cbfd54850b2d3bd
Marcin Siwonia <mahinek12@gmail.com>
2023-07-25 20:57:41 +0200

    Add final version

--- src/orders/dtos/create-order-dto.ts
+++ src/orders/dtos/create-order-dto.ts
@@ -3,13 +3,11 @@ import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 export class CreateOrderDTO {
   @IsNotEmpty()
   @IsUUID()
+  @IsString()
   productId: string;
 
   @IsNotEmpty()
+  @IsUUID()
   @IsString()
-  client: string;
-
-  @IsNotEmpty()
-  @IsString()
-  address: string;
+  clientId: string;
 }
